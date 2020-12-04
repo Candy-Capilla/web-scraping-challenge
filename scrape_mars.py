1fc6593042513a4540a552283ce180c563f3afb8
Candy Capilla <candycapilla@MacBook-Pro-3.local>
2020-12-04 16:36:47 -0500

    added more code to py

--- scrape_mars.py
+++ scrape_mars.py
@@ -22,5 +22,33 @@ def scrape():
     html = browser.html
     soup = bs(html, 'html.parser')
 
+#soupify page- find title
+    mars_soup = soup.find_all("div", class_= "content_title")
+    title= mars_soup[1].get_text()
+
+#soupify page- find paragraph
+    mars_soup = soup.find_all("div", class_= "article_teaser_body")
+    paragraph= mars_soup[0].get_text()
+
+#second url to scrape
+    url1 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url)
+    time.sleep(2)
+
+#find click links
+    browser.links.find_by_partial_text("FULL IMAGE")[0].click()
+    browser.links.find_by_partial_text("more info")[0].click()
+
+#use txt to find image
+    text_soup = soup.find_all("figure", class_= "lede")
+    figure= text_soup[0]
+    image_soup = figure.find_all("a", href = True)
+    image_soup[0]["href"]
+
+#define feature 
+    featured_image_url = "https://www.jpl.nasa.gov" + image_soup[0]["href"]
+
+#
+
 
 
