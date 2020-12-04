51cf74465bb172204cba289c1185cc982882a53c
Candy Capilla <candycapilla@MacBook-Pro-3.local>
2020-12-04 17:02:53 -0500

    added more code

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 import time
 
 
@@ -48,7 +49,36 @@ def scrape():
 #define feature 
     featured_image_url = "https://www.jpl.nasa.gov" + image_soup[0]["href"]
 
-#
+#scrape mars table 
+    df = pd.read_html("https://space-facts.com/mars/")
+    html_table = df[0].to_html()
+
+#scrape another url
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+    links=browser.links.find_by_partial_text("Hemisphere Enhanced")
+    hemisphere_image_urls= []
+
+#create  loop to repeat scrape
+    for l in range(len(links)):
+        browser.links.find_by_partial_text("Hemisphere Enhanced")[l].click()
+        time.sleep(2)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        downloads = soup.find_all("div", class_= "downloads")
+        figure= downloads[0]
+        pic_soup = figure.find_all("a", href = True)
+        #print(pic_soup[0]["href"])
+        hemisphere={}
+        hemisphere["img_url"]= pic_soup[0]["href"]
+        titles = soup.find_all("h2", class_= "title")
+        title= titles[0]
+        hemisphere["title"]= title.get_text()
+        hemisphere_image_urls.append(hemisphere)
+        browser.back()
+#close browser
+        browser.quit()
+
 
 
 
